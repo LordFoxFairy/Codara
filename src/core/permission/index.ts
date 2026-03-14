6f6270a173bfe3e58f66e7f3cd0114f5c14c67d8
LordFoxFairy <1479852727@qq.com>
2026-03-15 02:56:54 +0800

    feat(permission): implement core permission system

---
+++ src/core/permission/index.ts
@@ -0,0 +1,11 @@
+// src/core/permission/index.ts
+
+export * from './types';
+export * from './utils';
+export * from './handler';
+export * from './runtime/runtime';
+export * from './runtime/session-memory';
+export * from './policy/engine';
+export * from './policy/loader';
+export * from './policy/matcher';
+export * from './bash/analyzer';
