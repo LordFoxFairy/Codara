6ba030816aba3a928b0a5c9a48dedfe9b62d666c
LordFoxFairy <1479852727@qq.com>
2026-03-14 15:54:01 +0800

    refactor(permission): split bash permission helper

---
+++ src/core/middleware/permission/bash/index.ts
@@ -0,0 +1,4 @@
+export type {NormalizedBashCommand} from './types';
+export {bashSpecifierMatches, formatBashToolScopeExpression} from './scope';
+export {extractBashWritePathOperands} from './paths';
+export {normalizeBashCommandForMatching} from './normalize';
