cc06aa96899fe86787e70d6234c0e5f6f6dd51c5
LordFoxFairy <1479852727@qq.com>
2026-03-14 16:00:54 +0800

    refactor(permission): flatten bash helper layout

--- src/core/middleware/permission/bash/index.ts
+++
@@ -1,4 +0,0 @@
-export type {NormalizedBashCommand} from './types';
-export {bashSpecifierMatches, formatBashToolScopeExpression} from './scope';
-export {extractBashWritePathOperands} from './paths';
-export {normalizeBashCommandForMatching} from './normalize';
