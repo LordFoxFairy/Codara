ca0d9798f06836cbf0d3f7a08f530fad88fdc72f
LordFoxFairy <1479852727@qq.com>
2026-03-19 04:57:53 +0800

    feat(slack): ChannelPlugin 实现 + 测试

---
+++ src/integration/channel/slack/index.ts
@@ -0,0 +1,5 @@
+export {slackPlugin} from './plugin';
+export {SlackApi} from './api';
+export {SlackSocketModeClient} from './socket-mode';
+export {normalizeSlackMessage} from './plugin';
+export type {SlackAccountConfig} from './types';
