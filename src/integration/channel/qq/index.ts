9be8576854ab0e2e2c42d7c599ecb30174b2aace
LordFoxFairy <1479852727@qq.com>
2026-03-19 04:38:37 +0800

    feat(qq): barrel export + 测试

---
+++ src/integration/channel/qq/index.ts
@@ -0,0 +1,5 @@
+export {qqPlugin} from './plugin';
+export {OneBotWsClient} from './ws-client';
+export {normalizeOneBotMessage} from './plugin';
+export type {QQAccount} from './plugin';
+export type {QQAccountConfig, OneBotEvent, OneBotMessageEvent, OneBotMessageSegment, OneBotApiRequest, OneBotApiResponse} from './types';
